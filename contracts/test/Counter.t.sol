535e1d0d241b7d5aac1bde124792e22abc9ea76f
7md <39677084+dsaikvnbejskdf@users.noreply.github.com>
2025-11-17 15:07:10 +0300

    Update CounterTest with new tests and Solidity version
    .

--- contracts/test/Counter.t.sol
+++ contracts/test/Counter.t.sol
@@ -1,24 +1,35 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.24;
 
-import {Test} from "forge-std/Test.sol";
-import {Counter} from "../src/Counter.sol";
+import "forge-std/Test.sol";
+import "../src/Counter.sol";
 
 contract CounterTest is Test {
     Counter public counter;
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
+    }
+
+    function test_InitialValueIsZero() public {
+        assertEq(counter.number(), 0);
+    }
+
+    function test_SetNumber() public {
+        counter.setNumber(10);
+        assertEq(counter.number(), 10);
     }
 
     function test_Increment() public {
+        counter.setNumber(5);
         counter.increment();
-        assertEq(counter.number(), 1);
+        assertEq(counter.number(), 6);
     }
 
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
-        assertEq(counter.number(), x);
+    function test_IncrementAfterMultipleSets() public {
+        counter.setNumber(100);
+        counter.setNumber(50);
+        counter.increment();
+        assertEq(counter.number(), 51);
     }
 }
